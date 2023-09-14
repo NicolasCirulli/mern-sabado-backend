6aafb5a8b2128ed5d931f210b1d8c14d5ee7a31d
NicolasCirulli <nicolascirulli2@gmail.com>
2023-09-14 00:50:02 -0300

    remove space

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,25 +1,19 @@
 import passport from 'passport'
 import { Strategy,ExtractJwt } from 'passport-jwt'
 import User from '../models/User.js'
-
 const options = {
     jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey : process.env.SECRET_KEY
 }
-
 const fn = async ( jwt_payload, done ) => {
     try{
         const user = await User.findOne( { email: jwt_payload.email } )
-
         if( !user ){
             done( null, false )
         }
         done( null, user )
-
     }catch(err){
         done( err, false )
     }
-
 }
-
 export default passport.use( new Strategy( options, fn ) )
